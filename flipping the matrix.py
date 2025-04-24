fc3394c0ed066972bd99c98778f832e472069edf
bcatao92 <buno.silva@alumni.usp.br>
2025-04-23 23:46:26 -0300

    atualizando o flipping

--- flipping the matrix.py
+++ flipping the matrix.py
@@ -4,5 +4,22 @@ matrix = [[112, 42, 83, 119],
           [62, 98, 114, 108]]
 n2 = len(matrix)
 n = n2/2
-for i in matrix:
-    print(max(i))
+
+def inverteLinha(matriz,linha):
+    aux = []
+    for i in range(1,len(matriz[linha])):
+        aux.append(matrix[linha][-i])
+    aux.append(matrix[linha][0])
+    for i in range(len(aux)):
+        matriz[linha][i] = aux[i]
+    return matrix
+
+def inverteColuna(matriz,coluna):
+    aux = []
+    for i in range(1,len(matriz)):
+        aux.append(matriz[-i][coluna])
+    aux.append(matriz[0][coluna])
+    for i in range(len(aux)):
+        matriz[i][coluna] = aux[i]
+    return matriz
+
