d0466b341431495c2c17011f7d3c61f52842d016
bcatao92 <buno.silva@alumni.usp.br>
2025-04-28 11:50:36 -0300

    added sockMerchant

---
+++ sock_merchant.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    pares = {}
+    sum = 0
+    for meia in ar:
+        if meia not in pares:
+            pares[meia]=1
+        else:
+            pares[meia] = pares[meia]+1
+    for key,values in pares.items():
+        sum+=values//2
+    return sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
