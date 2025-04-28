ef6a94081e0cfa799a9116193ebd69907b2d7bc1
bcatao92 <buno.silva@alumni.usp.br>
2025-04-28 13:42:02 -0300

    add drawing book

---
+++ drawing book.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pageCount' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER p
+#
+
+def pageCount(n, p):
+    comeco = p//2
+    final = (n//2)-(p//2)
+    res = min (comeco, final)
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = int(input().strip())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
