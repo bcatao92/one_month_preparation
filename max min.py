e01d713deb4e1c072c47d73e20b456d0a49f883f
bcatao92 <buno.silva@alumni.usp.br>
2025-05-02 01:20:31 -0300

    blabla

--- max min.py
+++ max min.py
@@ -1,8 +1,8 @@
 k = 4
 arr = [1, 2, 3, 4, 10, 20, 30, 40, 100, 200]
 arr.sort()
-arrb = [arr[0:k+1]]
+arrb = []
 
-for i in range(len(arr)-k+1):
-    arrb.append(max(arr[i:i+k])-min(arr[i:i+k]))
+for i in range(len(arr) - k + 1):
+    arrb.append(max(arr[i : i + k]) - min(arr[i : i + k]))
 print(min(arrb))
