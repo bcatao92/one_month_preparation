b22c545736eb0a5847c54708f21f75a650005dc7
bcatao92 <buno.silva@alumni.usp.br>
2025-05-06 20:17:11 -0300

    anagram added

--- anagram.py
+++ anagram.py
@@ -1,26 +1,37 @@
-input = [
-    "abccde",
-    "aaabbb",
-    "ab",
-    "abc",
-    "mnop",
-    "xyyx",
-    "xaxbbbxx",
-    "fdhlvosfpafhalll",
-]
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'anagram' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
 
 def anagram(s):
     if len(s) % 2 != 0:
         return -1
     n = int(len(s) / 2)
-    aux1, aux2 = s[:n], s[n:]
-    aux3 = [i for i in aux1 if i not in aux2]
-    aux4 = [i for i in aux2 if i not in aux1]
-    print(aux1, aux2, aux3, aux4)
-    return len(aux3)
+    aux1, aux2, c = s[:n], s[n:],0
+    for e in set(aux1).intersection(aux2):
+        c += min(aux1.count(e), aux2.count(e))
+    return n-c    
 
-s = 'xyyx'
-s = 'fdhlvosfpafhalll'
-print(anagram(s))
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = anagram(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
