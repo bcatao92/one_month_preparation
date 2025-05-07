641aacb1246a242690ce808607474eddcec683c3
bcatao92 <buno.silva@alumni.usp.br>
2025-05-06 23:55:36 -0300

    palindromeindex

--- palindromeindex.py
+++ palindromeindex.py
@@ -1,10 +1,36 @@
-input = ['bcbc','aaab','baa','aaa']
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'palindromeIndex' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
 def palindromeIndex(s):
     for i in range(len(s)):
-       if s[i]!=s[-i-1]:
-           if s[i]==s[-i-2]:
-               return len(s)-1
-           return i
+        if s[i] != s[-i-1]:
+            if s[i+1:len(s)-i] == s[i+1:len(s)-i][::-1]:
+                return i
+            if s[i:len(s)-i-1] == s[i:len(s)-i-1][::-1]:
+                return len(s) - 1 - i
     return -1
-for i in input:
-    print(palindromeIndex(i))
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = palindromeIndex(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
