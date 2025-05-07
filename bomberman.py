dcc057f827325a5845fe5886d2a075853b646bb6
bcatao92 <buno.silva@alumni.usp.br>
2025-05-07 14:03:35 -0300

    bomberman add

---
+++ bomberman.py
@@ -0,0 +1,13 @@
+n = 3
+grid = ['.......', '...O...', '....O..','.......', 'OO.....', 'OO.....']
+
+def bomberman(n, grid):
+    i = 0
+    while i < n:
+        i+=1
+        for j in range(len(grid[0])):
+             
+    for line in grid:
+        print(line)
+    return 1
+bomberman(n,grid)
