687f4194ee2cda2458295ee835e148fd5434a925
bcatao92 <buno.silva@alumni.usp.br>
2025-05-08 17:54:59 -0300

    alteracoes bomberman

--- bomberman.py
+++ bomberman.py
@@ -1,82 +1,14 @@
-### Passa a matriz para dicionário
-def gridToDic(grid):
-    n = len(grid)
-    m = 0
-    try:
-        m = len(grid[0])
-    except:
-        print("FODEU")
-    finally:
-        m = 7
-#    if len(grid[0]):
-#        m = len(grid[0])
-    new_grid = {}
-    for i in range(n):
-        for j in range(m):
-.            new_grid[i, j] = grid[i][j]
-    return new_grid
+def encontraVizinhos(grid[a][b]):
+    
+    return -1
 
 
-### Popula o resto da grid com bombas, encontra vizinhos das bombas originais na grid e os explode junto com as bombas originais
-def encontraVizinhos(new_grid, guardabombas):
-    for k, v in new_grid.items():
-        if v == ".":
-            new_grid[k] = "O"
-    #####Encontra as bombas
-    for v in guardabombas.values():
-        #####Explode bombas e seus vizinhos
-        new_grid[v] = "."
-        a, b = v[0], v[1]
-        if (a + 1, b) in new_grid:
-            new_grid[a + 1, b] = "."
-        if (a - 1, b) in new_grid:
-            new_grid[a - 1, b] = "."
-        if (a, b + 1) in new_grid:
-            new_grid[a, b + 1] = "."
-        if (a, b - 1) in new_grid:
-            new_grid[a, b - 1] = "."
-    return new_grid
-
-
-def guardaBombas(grid):
-    i = 0
-    bombas = {}
-    for k, v in grid.items():
-        if v == "O":
-            bombas[i] = k
-            i += 1
-    return bombas
-
-
-# n = 3
-# grid = ["...", ".O.", "..."]
-
-
-def bomberMan(n, grid):
-
-    ans = []
-    segundos = 0
-    m = len(grid)
-    k = len(grid[0])
-    # novaGrid = gridToDic(grid)
-    while segundos < n:
-        grid = gridToDic(grid)
-        segundos += 1
-        bombasOriginais = guardaBombas(grid)
-        grid = encontraVizinhos(grid, bombasOriginais)
-        segundos += 2
-    # return grid
-    for i in range(m):
-        aux = ""
-        for j in range(k):
-            aux += grid[i, j]
-        ans.append(aux)
-    return ans
-
-
-# print(bomberMan(3,grid))
-
-grade = [".......", "...O.O.", "....O..", "..O....", "OO...OO", "OO.O..."]
-
-print(bomberMan(5, grade))
-# print(len(grid))
+def bomberMan(n,grid):
+    if n == 1 or (n-1)%4==0: return grid
+    gridfull = []
+    for i in range(len(grid)):
+        aux = []
+        for j in range(len(grid)):
+            aux.append(grid[i][j])
+        gridfull.append(aux)
+    if n%2 == 0: return gridfull
