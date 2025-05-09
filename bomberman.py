52aaa0c066401edec7ca4187829ca92b53313b0f
bcatao92 <buno.silva@alumni.usp.br>
2025-05-09 12:43:18 -0300

    bomberman completo

--- bomberman.py
+++ bomberman.py
@@ -1,35 +1,45 @@
-def encontraVizinhos(grid):
-    for a in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[a][j] == "O":
-                grid[a][j] = "."
-                if grid[a][j + 1] in grid:
-                    grid[a][j + 1] = "."
-                if grid[a][j - 1] in grid:
-                    grid[a][j - 1] = "."
-                if grid[a - 1][j] in grid:
-                    grid[a - 1][j + 1] = "."
-                if grid[a + 1][j] in grid:
-                    grid[a + 1][j] = "."
-    return grid
+#####
+#####           Completar encontraVizinhos
 
+def encontraVizinhos(grid):
+    n,m = len(grid), len(grid[0])
+    bombas = [['O']*m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == 'O':
+                bombas[i][j] = '.'
+                if i - 1 >= 0:
+                    bombas[i-1][j] = '.'
+                if i + 1 <= m:
+                    bombas[i+1][j] = '.' 
+                if j - 1 >= 0:
+                    bombas[i][j-1] = '.'
+                if j + 1 <= n:
+                    bombas[i][j+1] = '.'
+    for i in range(len(bombas)):
+        bombas[i] = ''.join(bombas[i])
+    return bombas
 
 def bomberMan(n, grid):
-    if n == 1 or (n - 1) % 4 == 0:
-        return grid
-    gridfull = []
-    for i in range(len(grid)):
-        aux = []
-        for j in range(len(grid[0])):
-            aux.append(grid[i][j])
-        gridfull.append(aux)
+    if n == 1: return grid
+    
+    x,m = len(grid), len(grid[0])
+    
     if n % 2 == 0:
-        return gridfull
-    if (n + 1) % 4 == 0:
-        return encontraVizinhos(grid)
+        return ['O'*m for _ in range(x)]
+    
+    explosao = encontraVizinhos(grid)
+    explosao2 = encontraVizinhos(explosao)
 
+    if n%4 == 1:
+        return explosao2
+    if n%4 == 3:
+        return explosao
 
-n = 3
-grid = ["...", ".O.", "..."]
-
-print(bomberMan(3,grid))
+n = 5
+grid = ['.......',
+'...O.O.',
+'....O..',
+'..O....',
+'OO...OO',
+'OO.O...']
