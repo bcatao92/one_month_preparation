076220bf54b199b7d1bf5dca74731243a751f115
bcatao92 <buno.silva@alumni.usp.br>
2025-05-13 01:59:35 -0300

    Sherlock and the valid string done

--- sherlock and the valid string.py
+++ sherlock and the valid string.py
@@ -1,22 +1,51 @@
-s = 'abc'
+#!/bin/python3
 
-def isValid(s):
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'isValid' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def isValid(s):  
     a = list(s)
     b = set(a)
     dici=[]
     for letra in b:
         dici.append(a.count(letra))
     dici.sort(reverse=True)
+
+    
     c = set(dici)
-    print(c)
+    #print(c)
     if(len(c)) == 1: return "YES"
     if len(c) > 2: return "NO"
-    if max(c)-min(c) > 1: return "NO"
+    if max(c)-min(c) > 1 and min(c) != 1: return "NO"
 
-    for i in range(len(dici)-1):
-        if dici[i] != dici[i+1]: 
-            if dici[i]-1 == dici[i+1]:
-                return "YES"
-            else: return "NO"
-    return "YES"
-print(isValid(s))
+    aux = dici
+    aux[0] -= 1
+    
+    if len(set(aux)) == 1: return "YES"
+    aux[0] += 1
+    dici[-1] -=1
+    if dici[-1] == 0:
+        del(dici[-1])
+    if len(set(dici)) == 1: return "YES"    
+    return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = isValid(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
